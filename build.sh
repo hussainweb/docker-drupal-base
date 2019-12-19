40b8a3b44c623e72d0e325be918d842927948cc4
hw <hussainweb@gmail.com>
2019-12-19 15:36:09 -0500

    Add a basic build script.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dir=$(dirname $0)
+
+set -ex
+
+docker pull php:7.3-apache-buster
+docker pull composer:1.9
+
+docker build -t hussainweb/drupal-base:php7.3 ${dir}/php7.3/apache/
+docker tag hussainweb/drupal-base:php7.3 hussainweb/drupal-base:latest
+
+docker push hussainweb/drupal-base:php7.3
+docker push hussainweb/drupal-base:latest
