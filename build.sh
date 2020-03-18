ecba3c3638d9956b01341fe96daa253c7a15856e
hw <hussainweb@gmail.com>
2020-03-18 15:15:49 -0400

    Upgrade to composer 1.10

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -ex
 export DOCKER_BUILDKIT=1
 
 docker pull php:7.3-apache-buster
-docker pull composer:1.9
+docker pull composer:1.10
 
 docker build -t hussainweb/drupal-base:php7.3 ${dir}/php7.3/apache/
 docker tag hussainweb/drupal-base:php7.3 hussainweb/drupal-base:latest
