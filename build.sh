6a2f3c9c4bf9286d9f41f49ae161addfe52a7389
hw <hussainweb@gmail.com>
2020-04-14 19:20:39 -0400

    Add PHP 7.4 support.

--- build.sh
+++ build.sh
@@ -7,10 +7,13 @@ set -ex
 export DOCKER_BUILDKIT=1
 
 docker pull php:7.3-apache-buster
+docker pull php:7.4-apache-buster
 docker pull composer:1.10
 
-docker build -t hussainweb/drupal-base:php7.3 ${dir}/php7.3/apache/
-docker tag hussainweb/drupal-base:php7.3 hussainweb/drupal-base:latest
+docker build -t hussainweb/drupal-base:php7.3 --build-arg PHP_VERSION=7.3-apache-buster ${dir}/php7.3/apache/
+docker build -t hussainweb/drupal-base:php7.4 --build-arg PHP_VERSION=7.4-apache-buster ${dir}/php7.4/apache/
+docker tag hussainweb/drupal-base:php7.4 hussainweb/drupal-base:latest
 
 docker push hussainweb/drupal-base:php7.3
+docker push hussainweb/drupal-base:php7.4
 docker push hussainweb/drupal-base:latest
