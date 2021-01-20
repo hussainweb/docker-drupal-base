509c28ffd9a1ae677f88fbca6e6628c6d920fe28
hw <hussainweb@gmail.com>
2021-01-20 13:36:44 -0500

    Add support for PHP 8.0 and Composer 2

--- build.sh
+++ build.sh
@@ -8,12 +8,15 @@ export DOCKER_BUILDKIT=1
 
 docker pull php:7.3-apache-buster
 docker pull php:7.4-apache-buster
-docker pull composer:1.10
+docker pull php:8.0-apache-buster
+docker pull composer:2.0
 
 docker build -t hussainweb/drupal-base:php7.3 --build-arg PHP_VERSION=7.3-apache-buster ${dir}/php7.3/apache/
 docker build -t hussainweb/drupal-base:php7.4 --build-arg PHP_VERSION=7.4-apache-buster ${dir}/php7.4/apache/
-docker tag hussainweb/drupal-base:php7.4 hussainweb/drupal-base:latest
+docker build -t hussainweb/drupal-base:php8.0 --build-arg PHP_VERSION=8.0-apache-buster ${dir}/php7.4/apache/
+docker tag hussainweb/drupal-base:php8.0 hussainweb/drupal-base:latest
 
 docker push hussainweb/drupal-base:php7.3
 docker push hussainweb/drupal-base:php7.4
+docker push hussainweb/drupal-base:php8.0
 docker push hussainweb/drupal-base:latest
--- clean.sh
+++ clean.sh
@@ -2,8 +2,10 @@
 
 docker rmi php:7.3-apache-buster
 docker rmi php:7.4-apache-buster
-docker rmi composer:1.10
+docker rmi php:8.0-apache-buster
+docker rmi composer:2.0
 
 docker rmi hussainweb/drupal-base:php7.3
 docker rmi hussainweb/drupal-base:php7.4
+docker rmi hussainweb/drupal-base:php8.0
 docker rmi hussainweb/drupal-base:latest
