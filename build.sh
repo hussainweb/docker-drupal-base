cb071447be221ef8cc65dc0abe141a799ac4a12b
hw <hussainweb@gmail.com>
2021-11-05 13:48:48 -0400

    Add libicu and use the latest version of composer 2

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export DOCKER_BUILDKIT=1
 docker pull php:7.3-apache-buster
 docker pull php:7.4-apache-buster
 docker pull php:8.0-apache-buster
-docker pull composer:2.0
+docker pull composer:2
 
 docker build -t hussainweb/drupal-base:php7.3 --build-arg PHP_VERSION=7.3-apache-buster ${dir}/php7.3/apache/
 docker build -t hussainweb/drupal-base:php7.4 --build-arg PHP_VERSION=7.4-apache-buster ${dir}/php7.4/apache/
--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 docker rmi php:7.3-apache-buster
 docker rmi php:7.4-apache-buster
 docker rmi php:8.0-apache-buster
-docker rmi composer:2.0
+docker rmi composer:2
 
 docker rmi hussainweb/drupal-base:php7.3
 docker rmi hussainweb/drupal-base:php7.4
