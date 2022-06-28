a8b39d7a55b792d12c64b91d6950cc8453cbe134
hw <hussainweb@gmail.com>
2022-06-28 08:05:05 -0400

    fix: remove PHP 7.3 and add PHP 8.1 support in scripts and CI

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ set -ex
 
 export DOCKER_BUILDKIT=1
 
-docker pull php:7.3-apache-buster
 docker pull php:7.4-apache-buster
 docker pull php:8.0-apache-buster
+docker pull php:8.1-apache-buster
 docker pull composer:2
 
 docker buildx create --use --name drupal-base-builder
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php7.4 --build-arg PHP_VERSION=7.3-apache-buster ${dir}/php7.3/apache/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php7.4 --build-arg PHP_VERSION=7.4-apache-buster ${dir}/php7.4/apache/
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.0 --tag hussainweb/drupal-base:latest --build-arg PHP_VERSION=8.0-apache-buster ${dir}/php7.4/apache/
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.0 --build-arg PHP_VERSION=8.0-apache-buster ${dir}/php7.4/apache/
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.1 --tag hussainweb/drupal-base:latest --build-arg PHP_VERSION=8.1-apache-buster ${dir}/php7.4/apache/
 
 docker buildx rm drupal-base-builder
--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-docker rmi php:7.3-apache-buster
 docker rmi php:7.4-apache-buster
 docker rmi php:8.0-apache-buster
+docker rmi php:8.1-apache-buster
 docker rmi composer:2
 
-docker rmi hussainweb/drupal-base:php7.3
 docker rmi hussainweb/drupal-base:php7.4
 docker rmi hussainweb/drupal-base:php8.0
+docker rmi hussainweb/drupal-base:php8.1
 docker rmi hussainweb/drupal-base:latest
