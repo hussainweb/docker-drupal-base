5bcb640276598d3e69cfdc87f4208e0c008fcd3c
hw <hussainweb@gmail.com>
2025-07-23 15:35:29 -0400

    feat!: add PHP 8.3 and 8.4 support, drop PHP 8.0
    - Update CI workflows, build scripts, and Dockerfile - Set latest tag to
    PHP 8.4 instead of 8.2

--- build.sh
+++ build.sh
@@ -6,15 +6,17 @@ set -ex
 
 export DOCKER_BUILDKIT=1
 
-docker pull php:8.0-apache-bullseye
 docker pull php:8.1-apache-bullseye
 docker pull php:8.2-apache-bullseye
+docker pull php:8.3-apache-bullseye
+docker pull php:8.4-apache-bullseye
 docker pull composer:2
 
 docker buildx create --use --name drupal-base-builder
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.0 --build-arg PHP_VERSION=8.0-apache-bullseye ${dir}/php8/apache/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.1 --build-arg PHP_VERSION=8.1-apache-bullseye ${dir}/php8/apache/
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.2 --tag hussainweb/drupal-base:latest --build-arg PHP_VERSION=8.2-apache-bullseye ${dir}/php8/apache/
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.2 --build-arg PHP_VERSION=8.2-apache-bullseye ${dir}/php8/apache/
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.3 --build-arg PHP_VERSION=8.3-apache-bullseye ${dir}/php8/apache/
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.4 --tag hussainweb/drupal-base:latest --build-arg PHP_VERSION=8.4-apache-bullseye ${dir}/php8/apache/
 
 docker buildx rm drupal-base-builder
--- clean.sh
+++ clean.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-docker rmi php:8.0-apache-bullseye
 docker rmi php:8.1-apache-bullseye
 docker rmi php:8.2-apache-bullseye
+docker rmi php:8.3-apache-bullseye
+docker rmi php:8.4-apache-bullseye
 docker rmi composer:2
 
-docker rmi hussainweb/drupal-base:php8.0
 docker rmi hussainweb/drupal-base:php8.1
 docker rmi hussainweb/drupal-base:php8.2
+docker rmi hussainweb/drupal-base:php8.3
+docker rmi hussainweb/drupal-base:php8.4
 docker rmi hussainweb/drupal-base:latest
