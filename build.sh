5407ffcdc4de3204f75d43f54782a932515c99c6
hw <hussainweb@gmail.com>
2026-01-30 10:31:12 -0500

    chore: Remove PHP 8.1 support
    PHP 8.1 is no longer supported as of this commit. This change removes its usage from the GitHub Actions workflow, README, and build/clean scripts.

--- build.sh
+++ build.sh
@@ -6,12 +6,10 @@ set -ex
 
 export DOCKER_BUILDKIT=1
 
-docker pull php:8.1-apache-bookworm
 docker pull php:8.2-apache-bookworm
 docker pull php:8.3-apache-bookworm
 docker pull php:8.4-apache-bookworm
 docker pull php:8.5-apache-bookworm
-docker pull php:8.1-fpm-alpine
 docker pull php:8.2-fpm-alpine
 docker pull php:8.3-fpm-alpine
 docker pull php:8.4-fpm-alpine
@@ -20,12 +18,10 @@ docker pull composer:2
 
 docker buildx create --use --name drupal-base-builder
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.1 --build-arg PHP_VERSION=8.1-apache-bookworm ${dir}/php8/apache-bookworm/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.2 --build-arg PHP_VERSION=8.2-apache-bookworm ${dir}/php8/apache-bookworm/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.3 --build-arg PHP_VERSION=8.3-apache-bookworm ${dir}/php8/apache-bookworm/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.4 --build-arg PHP_VERSION=8.4-apache-bookworm ${dir}/php8/apache-bookworm/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.5 --tag hussainweb/drupal-base:latest --build-arg PHP_VERSION=8.5-apache-bookworm ${dir}/php8/apache-bookworm/
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.1-alpine --build-arg PHP_VERSION=8.1-fpm-alpine ${dir}/php8/fpm-alpine/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.2-alpine --build-arg PHP_VERSION=8.2-fpm-alpine ${dir}/php8/fpm-alpine/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.3-alpine --build-arg PHP_VERSION=8.3-fpm-alpine ${dir}/php8/fpm-alpine/
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag hussainweb/drupal-base:php8.4-alpine --build-arg PHP_VERSION=8.4-fpm-alpine ${dir}/php8/fpm-alpine/
